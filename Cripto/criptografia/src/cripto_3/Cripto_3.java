875ceb365d0f7add5e882accb07c379cfe28adc7
Rayron Rodffer <44624642+Rodffer@users.noreply.github.com>
2019-10-21 21:30:35 -0300

    Add files via upload

---
+++ Cripto/criptografia/src/cripto_3/Cripto_3.java
@@ -0,0 +1,18 @@
+
+package cripto_3;
+
+/**
+ *
+ * @author aluno02.itb
+ */
+public class Cripto_3 {
+    
+    public static void main (String[] args){
+        String senha = "1234567";
+        System.out.println(CriptografiaMD5.criptografar(senha));
+        
+        senha = "1234567";
+        System.out.println(CriptografiaMD5.criptografar(senha));
+    }
+    
+}
